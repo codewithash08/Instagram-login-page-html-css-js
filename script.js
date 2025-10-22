2bd218dce988acb10e0fe277bb1b4cfb0bd1a6bc
Ash <ashwani555q@gmail.com>
2025-10-22 21:54:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+function myfunk (){
+    document.body.style.backgroundColor = "black";
+    document.getElementById("js").style.background = "SlateGray";
+    document.getElementById("js").style.borderColor = "white";
+    
+    document.querySelectorAll(".box5").forEach(e =>{
+        e.style.backgroundColor = "#";
+    
+    })
+
+}
+
+
+
+
+
+
+// let change =  document.getElementsByClassName("container")
+// console.log (change)
+// change[3].style.backgroundColor = "green";
+
